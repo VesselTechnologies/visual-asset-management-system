46f809601c594c6eba1b469df48e4f9de99a7aa1
bradencook <bradencook98@gmail.com>
2026-03-31 20:12:15 -0600

    changed it back

--- web/src/pages/auth/CognitoUsers.tsx
+++ web/src/pages/auth/CognitoUsers.tsx
@@ -213,8 +213,8 @@ export default function CognitoUsers() {
                                 </Alert>
                             )}
                             <FormField
-                                label="Username"
-                                constraintText="Required. Enter username (3-256 characters, alphanumeric with . + - @)"
+                                label="User ID"
+                                constraintText="Required. Enter user ID (3-256 characters, alphanumeric with . + - @)"
                                 errorText={userIdError}
                             >
                                 <Input
